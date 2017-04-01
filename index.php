29180ca7742fb8ea72165d1dfe699e4c3467a70a
S.Menz <mbasti@gmx.de>
2017-04-01 19:11:14 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Test Stage</title>
+	<link rel="stylesheet" href="/assets/css/app.css">
+</head>
+<body>
+	<div class="grid-container">
+		<div id="meta-navigation">
+			meta
+		</div>
+		<div id="logo">
+			<img src="https://www.fbo.de/wp-content/themes/fbo/images/logo-fbo-de.svg" width="100">
+		</div>
+		<div id="main-navigation">
+			<nav>
+				<a href="#">Start</a>
+				<a href="#">Item 1</a>
+				<a href="#">Item 2</a>
+				<a href="#">Item 3</a>
+			</nav>
+		</div>
+		<div id="left-sidebar">
+			<nav class="sidebar">
+				<a href="#">Start</a>
+				<a href="#">Item 1</a>
+				<a href="#">Item 2</a>
+				<a href="#">Item 3</a>
+			</nav>
+		</div>
+		<div id="content">
+				Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende Freude nach sich zieht? Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen? Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die Entscheidung trifft, eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz vermeidet, welcher keine daraus resultierende Freude nach sich zieht?Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur,
+		</div>
+		<!--<div id="right-sidebar">
+			<nav class="sidebar">
+				<a href="#">Start</a>
+				<a href="#">Item 1</a>
+				<a href="#">Item 2</a>
+				<a href="#">Item 3</a>
+			</nav>
+		</div>-->
+		<div id="footer">
+			<div id="footer-logo">
+				<img src="https://www.fbo.de/wp-content/themes/fbo/images/logo-fbo-de.svg" height="25">
+			</div>
+			<div id="footer-nav">
+				<nav>
+					<a href="#">Start</a>
+					<a href="#">Item 1</a>
+					<a href="#">Item 2</a>
+					<a href="#">Item 3</a>
+				</nav>
+			</div>
+			<div id="footer-partner">
+				<img src="https://www.fbo.de/wp-content/themes/fbo/images/logo-fbo-de.svg" height="25">
+				<img src="https://www.fbo.de/wp-content/themes/fbo/images/logo-fbo-de.svg" height="25">
+				<img src="https://www.fbo.de/wp-content/themes/fbo/images/logo-fbo-de.svg" height="25">
+			</div>
+			<div id="footer-copy">
+				copyright 2017
+			</div>
+			<div id="footer-meta">
+				<nav>
+					<a href="#">Start</a>
+					<a href="#">Item 1</a>
+					<a href="#">Item 2</a>
+					<a href="#">Item 3</a>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<script type="javacript" src="/assets/js/app.js"></script>
+</body>
+</html>
